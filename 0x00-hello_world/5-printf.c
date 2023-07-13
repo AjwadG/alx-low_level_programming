33872e484aa46913800a08358cac44ecb6df5397
AjwadG <ajwadelgandouz@yahoo.com>
2023-07-13 15:13:28 +0200

    task 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry level
+ *
+ * Description: print a string using printf
+ *
+ * Return: 0 if no error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
