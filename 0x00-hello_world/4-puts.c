63c22ab0d9142ea391263bb786c78f9bbcf421cc
AjwadG <ajwadelgandouz@yahoo.com>
2023-07-13 14:56:52 +0200

    task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Descriptoin: out puts a string using put
+ *
+ * Return:  0 if sucessful
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
