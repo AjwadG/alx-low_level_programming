cd6f35d238eebf970b561dfa793d335a7f17023d
AjwadG <ajwadelgandouz@yahoo.com>
2023-07-18 16:57:03 +0200

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * main - entrey point
+  *
+  * Description: using the function _putchar
+  * from the header file _putchar.c to print _putchar
+  *
+  * Return: 0 if success
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
