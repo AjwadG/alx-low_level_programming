e5b64312740d7d33f1f3e97ce8bd8dd64bee7e37
AjwadG <ajwadelgandouz@yahoo.com>
2023-07-18 19:51:13 +0200

    the sum of tow values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @n: the first number
+ * @x: the last number
+ *
+ * Description: adds two integers and returns the result.
+ *
+ * Return: the result of aading n and x
+ */
+
+int add(int n, int x)
+{
+	return (n + x);
+}
