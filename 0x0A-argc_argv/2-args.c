b47bd819756b18f674ec7503a1049d2c5aa5eabd
AjwadG <ajwadelgandouz@yahoo.com>
2023-08-04 20:00:00 +0200

    2. all comand line argumants

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+
+/**
+ * main - Entry point
+ * @argc: the number of comanline arguments
+ * @argv: a pointer to array contains coman line argumants
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
