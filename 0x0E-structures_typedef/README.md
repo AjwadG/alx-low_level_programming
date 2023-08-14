42b25f2d2591bf3782067a6db58c38b43328c25d
AjwadG <ajwadelgandouz@yahoo.com>
2023-08-14 18:59:47 +0200

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+0x0E. C - Structures, typedef its sad when u cant have classes
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+/**
+ * struct dog - a struct for dog info
+ * @name: name of the dog as string
+ * @age: age of the dog as float for some reasone
+ * @owner: name of the owner of the dog
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
