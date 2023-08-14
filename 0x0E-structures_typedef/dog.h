d01ebf4e8aca27eefb5875e648ddf9f87b56c467
AjwadG <ajwadelgandouz@yahoo.com>
2023-08-14 19:17:38 +0200

    2.I mean what if i am a Cat person

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct dog - a struct for dog info
  * @name: name of the dog as string
@@ -18,5 +19,6 @@ struct dog
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
