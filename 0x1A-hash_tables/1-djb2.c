bc6ad31ddd39183a5d4639fc353e9adf50ad4d67
AjwadG <ajwadelgandouz@yahoo.com>
2023-11-02 10:44:07 +0200

    1. djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_djb2 - djb2 hash funtion
+ *
+ * @str: pointer to first char of key
+ *
+ *
+ * Return: the hash number genrated
+ *
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;
+	}
+	return (hash);
+}
